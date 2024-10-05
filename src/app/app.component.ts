85aaa4da64ef73767d43dd106c94ab27af1326d6
Nithushan Janson <jansonnithusanofficial@gmail.com>
2024-10-05 15:02:37 +0530

    i dont like ngIf from angular legecy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './Header/Header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './user/dummy-users';
 import { TaskComponent } from './task/task.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent , UserComponent, TaskComponent],
+  imports: [HeaderComponent , UserComponent, TaskComponent,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
