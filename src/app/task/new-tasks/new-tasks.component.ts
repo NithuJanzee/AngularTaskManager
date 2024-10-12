a7c031c5acdc96db0542a576f4da131a7546fec8
Nithushan Janson <jansonnithusanofficial@gmail.com>
2024-10-13 00:06:47 +0530

    added ng model for two way binding

--- src/app/task/new-tasks/new-tasks.component.ts
+++ src/app/task/new-tasks/new-tasks.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-tasks',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-tasks.component.html',
   styleUrl: './new-tasks.component.css'
 })
 export class NewTasksComponent {
   @Output() Cancel = new EventEmitter<void>(); 
+  enteredTitle='';
+  enteredSummary='';
+  enteredDate='';
 
   oncancel(){
     this.Cancel.emit();
